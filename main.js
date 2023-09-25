16c145754a8342ae2179a146da95159adf0185e3
9walnut <kguho9202@gmail.com>
2023-09-25 17:22:10 +0900

    JS modified - skillbar delete

--- main.js
+++ main.js
@@ -57,40 +57,41 @@ arrowUp.addEventListener("click", () => {
   scrollIntoView("#Home");
 });
 
-class numberCounter {
-  constructor(target_frame, target_number) {
-    this.count = 0;
-    this.diff = 0;
-    this.target_count = parseInt(target_number);
-    this.target_frame = document.getElementById(target_frame);
-    this.timer = null;
-    this.counter();
-  }
-  counter() {
-    var self = this;
-    this.diff = this.target_count - this.count;
+// 스킬 숫자바 삭제로 인한 불필요
+// class numberCounter {
+//   constructor(target_frame, target_number) {
+//     this.count = 0;
+//     this.diff = 0;
+//     this.target_count = parseInt(target_number);
+//     this.target_frame = document.getElementById(target_frame);
+//     this.timer = null;
+//     this.counter();
+//   }
+//   counter() {
+//     var self = this;
+//     this.diff = this.target_count - this.count;
 
-    if (this.diff > 0) {
-      self.count += Math.ceil(this.diff / 5);
-    }
+//     if (this.diff > 0) {
+//       self.count += Math.ceil(this.diff / 5);
+//     }
 
-    this.target_frame.innerHTML = this.count
-      .toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+//     this.target_frame.innerHTML = this.count
+//       .toString()
+//       .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-    if (this.count < this.target_count) {
-      this.timer = setTimeout(function () {
-        self.counter();
-      }, 20);
-    } else {
-      clearTimeout(this.timer);
-    }
-  }
-}
+//     if (this.count < this.target_count) {
+//       this.timer = setTimeout(function () {
+//         self.counter();
+//       }, 20);
+//     } else {
+//       clearTimeout(this.timer);
+//     }
+//   }
+// }
 
-new numberCounter("counter1", 100);
-new numberCounter("counter2", 70);
-new numberCounter("counter3", 70);
+// new numberCounter("counter1", 100);
+// new numberCounter("counter2", 70);
+// new numberCounter("counter3", 70);
 
 // Projects
 const workBtnContainer = document.querySelector(".work_categories");
